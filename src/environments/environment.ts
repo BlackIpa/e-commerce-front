680e7012236fac60791e1d92c35d8baf5345674a
BlackIpa <augustyniak.p@Gmail.com>
2022-03-08 12:20:22 +0100

    closing of ecommerce project v2.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+	// add your custom environment configs
+
+	luv2shopApiUrl: "https://localhost:8443/api"
 };
 
 /*
